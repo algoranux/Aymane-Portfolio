13412ba72bb79fffda87b3543af825d703151d35
Aymane.coder <153773550+coderaymane@users.noreply.github.com>
2024-12-14 08:16:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,19 +23,3 @@ navLinks.addEventListener('click', (event) => {
         menuToggle.classList.remove('active');
     }
 });
-
-document.addEventListener('contextmenu', event=> event.preventDefault()); 
-document.onkeydown = function(e) { 
-if(event.keyCode == 123) { 
-return false; 
-} 
-if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){ 
-return false; 
-} 
-if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){ 
-return false; 
-} 
-if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){ 
-return false; 
-} 
-} 
